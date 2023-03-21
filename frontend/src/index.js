9eb9e6fdde20c7495fcef39aafbe0d4d802c8efc
phamm25 <anhminh23252001@gmail.com>
2023-03-21 19:37:34 -0400

    update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-icons/font/bootstrap-icons.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
@@ -18,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { value: 0 }, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { value: state.value + 1 + action.someValue };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer, { value: 0 });
+
+store.dispatch({
+  type: "ADD",
+  someValue: 10,
+});
+
+console.log(store.getState());
+
+export default store;
+

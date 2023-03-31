78268952f4eeaaff1c52468c7defc456dd621d6a
phamm25 <anhminh23252001@gmail.com>
2023-03-31 17:23:49 -0400

    new

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 const app = express();
-const port = 5000;
+
+const httpServer = createServer(app);
+global.io = new Server(httpServer);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -39,7 +43,6 @@ app.use((error, req, res, next) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const PORT = process.env.PORT || 5000;
 
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));

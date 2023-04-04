70128044dc67c282d7d0f324684e9f1131ab7498
phamm25 <anhminh23252001@gmail.com>
2023-04-03 23:25:48 -0400

    new

--- frontend/src/components/user/UserChatComponent.js
+++ frontend/src/components/user/UserChatComponent.js
@@ -1,24 +1,49 @@
 import "../../chats.css";
 import { useEffect, useState } from "react";
 import socketIOClient from "socket.io-client";
+import { useSelector } from "react-redux";
 
 const UserChatComponent = () => {
+  const [socket, setSocket] = useState(false);
+  //   let chat = [
+  //       {"client": "msg"},
+  //       {"client": "msg"},
+  //       {"admin": "msg"},
+  //   ]
+  const [chat, setChat] = useState([]);
 
-    const [socket, setSocket] = useState(false);
-    useEffect(() => {
-        const socket = socketIOClient();
-        setSocket(socket);
-        return () => socket.disconnect();
-    }, [])
+  const userInfo = useSelector((state) => state.userRegisterLogin.userInfo);
 
-    const clientSubmitChatMsg = (e) => {
-        if (e.keyCode && e.keyCode !== 13) {
-            return
-        }
-        socket.emit("client sends message", "message from client");
+  useEffect(() => {
+    if (!userInfo.isAdmin) {
+      const socket = socketIOClient();
+      setSocket(socket);
+      return () => socket.disconnect();
     }
+  }, [userInfo.isAdmin]);
 
-  return (
+  const clientSubmitChatMsg = (e) => {
+    if (e.keyCode && e.keyCode !== 13) {
+      return;
+    }
+    const msg = document.getElementById("clientChatMsg");
+    let v = msg.value.trim();
+    if (v === "" || v === null || v === false || !v) {
+      return;
+    }
+    socket.emit("client sends message", v);
+    setChat((chat) => {
+      return [...chat, { client: v }];
+    });
+    msg.focus();
+    setTimeout(() => {
+         msg.value = "";
+         const chatMessages = document.querySelector(".cht-msg");
+         chatMessages.scrollTop = chatMessages.scrollHeight;
+    }, 200)
+  };
+
+  return !userInfo.isAdmin ? (
     <>
       <input type="checkbox" id="check" />
       <label className="chat-btn" htmlFor="check">
@@ -32,29 +57,38 @@ const UserChatComponent = () => {
         </div>
         <div className="chat-form">
           <div className="cht-msg">
-            {Array.from({ length: 20 }).map((_, id) => (
+            {chat.map((item, id) => (
               <div key={id}>
-                <p>
-                  <b>You wrote:</b> Hello, world! This is a toast message.
-                </p>
-                <p className="bg-primary p-3 ms-4 text-light rounded-pill">
-                  <b>Support wrote:</b> Hello, world! This is a toast message.
-                </p>
+                {item.client && (
+                  <p>
+                    <b>You wrote:</b> {item.client}
+                  </p>
+                )}
+                {item.admin && (
+                  <p className="bg-primary p-3 ms-4 text-light rounded-pill">
+                    <b>Support wrote:</b> {item.admin}
+                  </p>
+                )}
               </div>
             ))}
           </div>
           <textarea
-          onKeyUp={(e) => clientSubmitChatMsg(e)}
+            onKeyUp={(e) => clientSubmitChatMsg(e)}
             id="clientChatMsg"
             className="form-control"
             placeholder="Your Text Message"
           ></textarea>
 
-          <button onClick={(e) => clientSubmitChatMsg(e)} className="btn btn-success btn-block">Submit</button>
+          <button
+            onClick={(e) => clientSubmitChatMsg(e)}
+            className="btn btn-success btn-block"
+          >
+            Submit
+          </button>
         </div>
       </div>
     </>
-  );
+  ) : null;
 };
 
 export default UserChatComponent;

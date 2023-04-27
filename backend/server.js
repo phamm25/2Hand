b9282c6c542c5dcb881d89b0ebaed0bf40d92434
phamm25 <anhminh23252001@gmail.com>
2023-04-26 22:27:34 -0400

    new

--- backend/server.js
+++ backend/server.js
@@ -100,7 +100,7 @@ app.use((error, req, res, next) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;ss
 
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 

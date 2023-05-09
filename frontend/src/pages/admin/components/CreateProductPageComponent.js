f4ee2f31251545f56feb8b287ed8627384a8eb25
phamm25 <anhminh23252001@gmail.com>
2023-05-08 22:32:29 -0400

    new

--- frontend/src/pages/admin/components/CreateProductPageComponent.js
+++ frontend/src/pages/admin/components/CreateProductPageComponent.js
@@ -86,7 +86,7 @@ const CreateProductPageComponent = ({
             error: er.response.data.message
               ? er.response.data.message
               : er.response.data,
-          });
+          });ss
         });
     }
 
